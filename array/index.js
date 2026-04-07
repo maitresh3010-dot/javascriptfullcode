fd2d65b0d3ced7b3df1aebc40a948be8ee9166af
Maitresh Uke <maitershuke@gmail.com>
2026-04-07 21:14:31 +0530

    yee he pura array ka saar

---
+++ array/index.js
@@ -0,0 +1,48 @@
+let arr = [1,2,3,4,5,10]
+
+// console.log(arr)
+// arr.push(20,30)
+
+// console.log(arr)
+
+
+// arr.unshift(5,6,786)
+
+
+// console.log(arr)
+
+
+
+
+
+ 
+
+// arr.shift(1,2,3,4)
+// console.log(arr)
+
+
+// arr.pop()
+// console.log(arr)
+
+// console.log(arr.sort( function (a,b){
+//     return a+b
+// }))
+
+// arr.forEach(function (ele, index){
+//     console.log(ele, index)
+// })
+
+
+// let maps = arr.map(function (value){
+//     return  value*2
+// })
+
+// console.log(maps)
+
+
+let checkpositivevalue =(checkvalue)=>{
+    return checkvalue>0;
+}
+let arr1 = [-1,34,44,-33]
+console.log(arr1.filter(checkpositivevalue))
+
